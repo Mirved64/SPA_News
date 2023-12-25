8aad991c8d8ec31e57694585b6ace8b19ad02a36
Mirved64 <medmixal@yahoo.com>
2023-12-25 15:11:29 +0400

    chore: rework home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 'use client'
 import { useEffect } from 'react'
+import styles from './styles.module.css'
+
 import { ArticleCard } from '@components/article-card'
 import { fetchArticles } from '@lib/redux'
+import { Condition } from '@ui'
 import { useAppDispatch, useAppSelector } from '@utils'
 
 const Home = () => {
@@ -14,18 +17,19 @@ const Home = () => {
 
   return (
     <>
-      {isLoading && <h1>Wait please!</h1>}
-      {articles &&
-        articles.map((article) => (
-          <ArticleCard
-            key={article.id}
-            id={article.id}
-            apiUrl={article.apiUrl}
-            webTitle={article.webTitle}
-            webPublicationDate={article.webPublicationDate}
-            blocks={article.blocks}
-          />
-        ))}
+      <div className={styles.wrapperContent}>
+        <Condition match={!isLoading}>
+          {articles.map((article) => (
+            <ArticleCard
+              key={article.id}
+              id={article.id}
+              webTitle={article.webTitle}
+              webPublicationDate={article.webPublicationDate}
+              blocks={article.blocks}
+            />
+          ))}
+        </Condition>
+      </div>
     </>
   )
 }
