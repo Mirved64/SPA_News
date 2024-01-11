e4aee8d6c55be0d96aef52493ad2c40dc9c19a20
Mirved64 <medmixal@yahoo.com>
2024-01-11 17:48:38 +0400

    chore: remove condition

--- src/ui/condition/condition.component.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react'
-import { ConditionProps } from './condition.interfaces'
-
-export const Condition: FC<ConditionProps> = ({ match, children }) => {
-  if (!match) return null
-
-  // eslint-disable-next-line
-  return <>{children}</>
-}
